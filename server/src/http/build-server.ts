9392ee2249e3f525e0a711510788395ccb18cf28
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2024-12-31 19:52:24 -0300

    feat(server): integrate Zod for request validation and response serialization
    Enhance server configuration by incorporating Zod validators and serializers,
    improving type safety and data integrity in API interactions.

--- server/src/http/build-server.ts
+++ server/src/http/build-server.ts
@@ -1,8 +1,12 @@
-import fastify, { type FastifyInstance } from 'fastify'
+import { fastify, type FastifyInstance } from 'fastify'
+import { validatorCompiler, serializerCompiler} from 'fastify-type-provider-zod'
 import { routes } from './routes'
 
 export function buildServer(): FastifyInstance {
   const server = fastify()
+
+  server.setValidatorCompiler(validatorCompiler)
+  server.setSerializerCompiler(serializerCompiler)
 
   routes(server)
 
