04aa5e4777f1545952e4dc97a66f7b3e4b70dcd0
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2024-12-31 19:55:43 -0300

    feat(server): integrate CORS and Swagger documentation
    - Add CORS support to allow cross-origin requests
    - Implement Swagger for API documentation and interactive UI
    - Configure Swagger with basic API information
    - Expose documentation at '/docs' endpoint
    ```

--- server/src/http/build-server.ts
+++ server/src/http/build-server.ts
@@ -1,14 +1,40 @@
 import { fastify, type FastifyInstance } from 'fastify'
-import { validatorCompiler, serializerCompiler} from 'fastify-type-provider-zod'
+import {
+  validatorCompiler,
+  serializerCompiler,
+  jsonSchemaTransform,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
 import { routes } from './routes'
 
 export function buildServer(): FastifyInstance {
-  const server = fastify()
+  const server = fastify().withTypeProvider<ZodTypeProvider>()
 
   server.setValidatorCompiler(validatorCompiler)
   server.setSerializerCompiler(serializerCompiler)
 
-  routes(server)
+  server.register(fastifyCors, {
+    origin: '*',
+  })
+
+  server.register(fastifySwagger, {
+    openapi: {
+      info: {
+        title: 'Medify API',
+        version: '1.0.0',
+      },
+    },
+    transform: jsonSchemaTransform,
+  })
+
+  server.register(fastifySwaggerUi, {
+    routePrefix: '/docs',
+  })
+
+  server.register(routes)
 
   return server
 }
