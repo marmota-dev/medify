c811910a9eda03a64a17c241cc68c55189c0c062
Guilherme Buys <131329633+BuysDev@users.noreply.github.com>
2024-12-31 16:10:40 -0300

    feat(login): creating the login system

--- server/src/http/schemas/pharmacySchema.ts
+++ server/src/http/schemas/pharmacySchema.ts
@@ -1,22 +1,15 @@
 import { z } from 'zod'
 
 export const userSchema = z.object({
-  id: z
-    .string(),
-  name: z
-    .string()
-    .min(1, 'O nome é obrigatório'),
-  address: z
-    .string()
-    .min(1, 'O endereço é obrigatório'),
-  phoneNumber: z
-    .string()
-    .regex(/^\+?\d{10,15}$/, 'Telefone inválido'),
+  id: z.string(),
+  name: z.string().min(1, 'O nome é obrigatório'),
+  address: z.string().min(1, 'O endereço é obrigatório'),
+  phoneNumber: z.string().regex(/^\+?\d{10,15}$/, 'Telefone inválido'),
   operatingHours: z
     .string()
-    .regex(/^\d{2}:\d{2} - \d{2}:\d{2}$/, 'Horário de funcionamento deve estar no formato HH:MM - HH:MM'),
-  email: z
-    .string()
-    .email()
-    .min(1, 'O e-mail é obrigatório'),
+    .regex(
+      /^\d{2}:\d{2} - \d{2}:\d{2}$/,
+      'Horário de funcionamento deve estar no formato HH:MM - HH:MM'
+    ),
+  email: z.string().email().min(1, 'O e-mail é obrigatório'),
 })
--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,7 +1,12 @@
 import { buildServer } from './build-server'
 import { env } from '../env'
+import { PrismaClient } from '@prisma/client'
 
 const server = buildServer()
+
+export const prisma = new PrismaClient({
+  log: ['query']
+})
 
 server
   .listen({ port: env.PORT })
