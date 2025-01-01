ef238868c3f23dd65cc11cebdd260e16135cdb40
Guilherme Buys <131329633+BuysDev@users.noreply.github.com>
2025-01-01 14:24:16 -0300

    feat: removing the login route

--- server/src/http/routes.ts
+++ server/src/http/routes.ts
@@ -1,65 +1,39 @@
 import type { FastifyInstance } from 'fastify'
-import * as jwt from 'jsonwebtoken'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { userSchema } from './schemas/userSchema'
 import { prisma } from '../database/prisma'
-import { compareSync } from 'bcrypt'
-
-import { env } from '../env'
+import { hash } from 'bcrypt'
 
 export async function routes(server: FastifyInstance) {
   server.get('/', async (request, reply) => {
     return { message: 'Hello World!' }
   })
 
-  server.withTypeProvider<ZodTypeProvider>().post('api/login', { schema: { body: userSchema } }, async (request, reply) => {
-      const { email, password } = request.body
+  server.withTypeProvider<ZodTypeProvider>().post('api/register', { schema: { body: userSchema } }, async (request, reply) => {
+    const { email, name, password } = request.body
 
-      if (email === "") {
-        return reply.status(400).send({ error: 'O e-mail é obrigatório' })
-      } else if (password === "") {
-        return reply.status(400).send({ error: 'A senha é obrigatória' })
-      } else if (email === "" && password === "") {
-        return reply.status(400).send({ error: 'O e-mail e a senha são obrigatórios' })
-      }
+    let user = await prisma.user.findFirst({ where: { email } })
 
-      if (typeof email !== "string") {
-        return reply.status(400).send({ error: 'O tipo do e-mail deve ser um texto' })
-      } else if (typeof password !== "string") {
-        return reply.status(400).send({ error: 'O tipo da senha deve ser um texto' })
-      } else if (typeof email !== "string" && typeof password !== "string") {
-        return reply.status(400).send({ error: 'Os tipos do e-mail e da senha devem ser textos' })
-      }
-
-      if (email.length > 255) {
-        return reply.status(400).send({ error: 'O limite de caracteres do e-mail é 255' })
-      }
-
-      if (password.length > 255) {
-        return reply.status(400).send({ error: 'O limite de caracteres da senha é 255' })
-      }
-
-      if (password.length < 8) {
-        return reply.status(400).send({ error: 'O mínimo de caracteres da senha é 8' })
-      }
-
-      let user = await prisma.user.findFirst({ where: { email } })
-
-      if (!user) {
-        return reply.status(401).send({ error: 'O usuário não existe' })
-      }
-
-      if (!compareSync(password, user.password)) {
-        return reply.status(401).send({ error: 'Senha incorreta' })
-      }
-
-      const token = jwt.sign({
-        userId: user.id,
-      }, env.JWT_SECRET_KEY)
-
-      reply.send({ message: { user, token } })
+    if (user) {
+      return reply.send({ error: "E-mail já cadastrado no sistema"})
     }
-  )
+
+    const hashPassword = await hash(password, 10)
+
+    const newUser = await prisma.user.create({
+      data: {
+        email: email,
+        password: hashPassword,
+        name: name
+      }
+    })
+
+    const { password:_, ...userRegister } = newUser
+
+    return reply.status(201).send({
+      user: JSON.stringify(userRegister)
+    })
+  })
 
   server.post('api/pharmacy/add', async (request, reply) => {
     reply.status(201).send({ message: 'Pharmacy added successfully' })
