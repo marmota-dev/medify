35e1224cbc25d2dc7876c21a68a4739402e025a9
Guilherme Buys <131329633+BuysDev@users.noreply.github.com>
2025-01-02 23:48:51 -0300

    teste

--- server/src/http/build-server.ts
+++ server/src/http/build-server.ts
@@ -1,10 +1,35 @@
 import fastify, { type FastifyInstance } from 'fastify'
 import { routes } from './routes'
+import { validatorCompiler, serializerCompiler, jsonSchemaTransform, type ZodTypeProvider } from 'fastify-type-provider-zod'
+import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
 
 export function buildServer(): FastifyInstance {
-  const server = fastify()
+  const server = fastify().withTypeProvider<ZodTypeProvider>()
 
-  routes(server)
+  server.setValidatorCompiler(validatorCompiler)
+  server.setSerializerCompiler(serializerCompiler)
+
+  server.register(fastifyCors, {
+    origin: '*',
+  })
+
+  server.register(fastifySwagger, {
+    openapi: {
+      info: {
+        title: 'Medify API',
+        version: '1.0.0',
+      },
+    },
+    transform: jsonSchemaTransform,
+  })
+
+  server.register(fastifySwaggerUi, {
+    routePrefix: '/docs',
+  })
+
+  server.register(routes)
 
   return server
 }
