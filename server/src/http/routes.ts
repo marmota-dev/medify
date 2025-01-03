9a85eaf09e4e8b41ce9437c14b4afd65c28e7972
Guilherme Buys <131329633+BuysDev@users.noreply.github.com>
2025-01-03 00:27:41 -0300

    feat: login system done successfully

--- server/src/http/routes.ts
+++ server/src/http/routes.ts
@@ -6,62 +6,74 @@ import { prisma } from '../database/prisma'
 import { compare } from 'bcrypt'
 
 import { env } from '../env'
-import z from 'zod'
 
 export async function routes(server: FastifyInstance) {
   server.get('/', async (request, reply) => {
     return { message: 'Hello World!' }
   })
 
-  server.withTypeProvider<ZodTypeProvider>().post(
-    '/api/login',
-    {
-      schema: {
-        tags: ['auth'],
-        summary: 'This route is responsible for authenticating the user',
-        body: userSchema,
-        response: {
-          200: z.object({
-            access_token: z.string(),
-            token_type: z.literal('Bearer'),
-            expires_in: z.number(),
-            user: z.object({
-              id: z.number(),
-              name: z.string(),
-              email: z.string(),
-            }),
-          }),
-          401: z.object({
-            error: z.string(),
-          }),
-        },
-      },
-    },
-    async (request, reply) => {
-      const { email, password } = request.body
+  server
+    .withTypeProvider<ZodTypeProvider>()
+    .post(
+      '/api/register',
+      { schema: { body: userSchema } },
+      async (request, reply) => {
+        const { email, password } = request.body
 
-      const user = await prisma.user.findFirst({ where: { email } })
+        const errors: object[] = []
 
-      if (!user || !compare(password, user.password)) {
-        return reply.status(401).send({ error: 'Credenciais inválidas' })
+        if (email === '') {
+          errors.push({ error: 'O e-mail é obrigatório' })
+        }
+
+        if (password === '') {
+          errors.push({ error: 'A senha é obrigatória' })
+        }
+
+        if (typeof email !== 'string') {
+          errors.push({ error: 'O e-mail deve ser um texto' })
+        }
+
+        if (typeof password !== 'string') {
+          errors.push({ error: 'A senha deve ser um texto' })
+        }
+
+        if (email.length > 255) {
+          errors.push({ error: 'O limite de caracteres do e-mail é 255' })
+        }
+
+        if (password.length > 255 || password.length < 8) {
+          errors.push({
+            error: 'O mínimo de caracteres da senha é 8 e o limite é 255',
+          })
+        }
+
+        if (errors.length >= 1) {
+          return reply.status(400).send({ errors: JSON.stringify(errors) })
+        }
+
+        const user = await prisma.user.findFirst({ where: { email } })
+
+        if (!user) {
+          return reply.status(401).send({ error: 'Usuário não encontrado' })
+        }
+
+        if (!compare(password, user.password)) {
+          return reply.status(401).send({ error: "As senhas não são iguais" })
+        }
+
+        const token = jwt.sign({
+          userId: user.id
+        }, env.JWT_SECRET_KEY)
+
+        const { password:_, ...userLogin } = user
+
+        return reply.status(200).send({
+          token: token,
+          user: userLogin
+        })
       }
-
-      const token = jwt.sign({ userId: user.id }, env.JWT_SECRET_KEY, {
-        expiresIn: '1h',
-      })
-
-      return reply.status(200).send({
-        access_token: token,
-        token_type: 'Bearer',
-        expires_in: 3600,
-        user: {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-        },
-      })
-    }
-  )
+    )
 
   server.post('/api/pharmacy/add', async (request, reply) => {
     reply.status(201).send({ message: 'Pharmacy added successfully' })
--- server/src/http/schemas/userSchema.ts
+++ server/src/http/schemas/userSchema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const userSchema = z.object({
-  email: z.string().email().max(255, 'O limite de caracteres do e-mail é 255'),
+  email: z.string().email('O e-mail digitado deve ser um e-mail válido').max(255, 'O limite de caracteres do e-mail é 255'),
   password: z
     .string()
     .min(8, 'A senha deve ter no mínimo 8 caracteres')
