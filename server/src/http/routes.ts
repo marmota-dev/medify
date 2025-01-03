3ba7b41cb5bdffd920c73ab55169bbbc6707327b
Guilherme Buys <131329633+BuysDev@users.noreply.github.com>
2025-01-03 00:45:59 -0300

    fix: fixing redundance

--- server/src/http/routes.ts
+++ server/src/http/routes.ts
@@ -17,64 +17,8 @@ export async function routes(server: FastifyInstance) {
       async (request, reply) => {
         const { email, password, name, confirm_password } = request.body
 
-        const errors: object[] = []
-
-        if (email === '') {
-          errors.push({ error: 'O e-mail é obrigatório' })
-        }
-
-        if (password === '') {
-          errors.push({ error: 'A senha é obrigatória' })
-        }
-
-        if (name === '') {
-          errors.push({ error: 'O nome é obrigatório' })
-        }
-
-        if (confirm_password === '') {
-          errors.push({ error: 'A confirmação da senha é obrigatória' })
-        }
-
-        if (typeof email !== 'string') {
-          errors.push({ error: 'O e-mail deve ser um texto' })
-        }
-
-        if (typeof password !== 'string') {
-          errors.push({ error: 'A senha deve ser um texto' })
-        }
-
-        if (typeof name !== 'string') {
-          errors.push({ error: 'O nome deve ser um texto' })
-        }
-
-        if (typeof confirm_password !== 'string') {
-          errors.push({ error: 'A confirmação da senha deve ser um texto' })
-        }
-
-        if (email.length > 255) {
-          errors.push({ error: 'O limite de caracteres do e-mail é 255' })
-        }
-
-        if (password.length > 255 || password.length < 8) {
-          errors.push({
-            error: 'O mínimo de caracteres da senha é 8 e o limite é 255',
-          })
-        }
-
-        if (name.length < 3 || name.length > 255) {
-          errors.push({
-            error: 'O mínimo de caracteres do nome é 3 e o limite é 255',
-          })
-        }
-
         if (password !== confirm_password) {
-          errors.push({
-            error: "As senhas devem ser iguais"
-          })
-        }
-
-        if (errors.length >= 1) {
-          return reply.status(400).send({ errors: JSON.stringify(errors) })
+          return reply.status(409).send({ error: "As senhas não são iguais"})
         }
 
         const user = await prisma.user.findFirst({ where: { email } })
