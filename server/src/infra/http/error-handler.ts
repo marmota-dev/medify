e6dcc5bd920ebc14e0bfacf9f509144277118e14
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2025-01-07 10:10:01 -0300

    refactor(error-handler): streamline HTTP error responses
    Consolidate error handling for BadRequest and Unauthorized errors,
    improving code consistency and reducing duplication. This change
    enhances the maintainability of the error-handler module while
    preserving its functionality.

--- server/src/infra/http/error-handler.ts
+++ server/src/infra/http/error-handler.ts
@@ -5,19 +5,11 @@ import { Unauthorized } from './errors/Unauthorized'
 type FastifyErrorHandler = FastifyInstance['errorHandler']
 
 export const errorHandler: FastifyErrorHandler = (error, request, reply) => {
-  if (error instanceof BadRequest) {
+  if (error instanceof BadRequest || error instanceof Unauthorized) {
     return reply.status(error.statusCode).send({
       error: error.name,
       message: error.message,
       statusCode: error.statusCode,
-    })
-  }
-
-  if (error instanceof Unauthorized) {
-    return reply.status(error.statusCode).send({
-      statusCode: error.statusCode,
-      error: error.name,
-      message: error.message,
     })
   }
 
